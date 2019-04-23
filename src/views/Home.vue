4bcf4220abebfc6b76f8ecdf8e3aa5a0d2527615
xu15720036341 <pengfei.xu@xblgroup.com>
2019-04-23 11:20:21 +0800

    页面样式搞定

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-container>
+      <el-header>
+        <!-- 顶部 -->
+        <Header></Header>
+      </el-header>
+      <el-container>
+        <!-- 侧边栏 -->
+        <el-aside width="200px">
+          <Aside></Aside>
+        </el-aside>
+        <el-main>
+          <Content></Content>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/views/header";
+import Aside from "@/views/aside";
+import Content from "@/views/content/content";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Header,
+    Aside,
+    Content
   }
 };
 </script>
---
+++ src/views/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header">
+    API测试工具
+  </div>
+</template>
+
+<style lang="less" scoped>
+.header {
+  width: 100%;
+  height: 50px;
+  background-color: #ccc;
+}
+</style>
